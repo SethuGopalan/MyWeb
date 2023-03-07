989d112d2737856e6cf3d6c0500d6645241c0ecb
Sethu Gopalan <sethugopalan@outlook.com>
2023-03-06 19:13:20 -0500

    edit vistapot

--- application.py
+++ application.py
@@ -8,7 +8,8 @@ from dash_iconify import DashIconify
 
 
 application = Dash(__name__, plugins=[dl.plugins.pages],
-                external_stylesheets=[dbc.themes.SLATE])
+                external_stylesheets=[dbc.themes.SLATE],meta_tags=[{'name': 'viewport',
+                        'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 server=application.server
                 
 # app.layout = html.Div([html.Script(**{"data-url": "https://platform.linkedin.com/badges/js/profile.js"}, type="IN/Share")])
